d842d76b83ccbc31653ef9b21757711e2c5089e7
AlexandreBitelo <142456940+AlexandreBitelo@users.noreply.github.com>
2023-09-16 21:59:48 -0300

    Add files via upload

---
+++ Repositório Aulas/aula5-noite/README.md
@@ -0,0 +1,2 @@
+# aula5-noite
+Conteúdo e exercícios da aula 5 de JS
---
+++ Repositório Aulas/aula5-noite/index.js
@@ -0,0 +1,37 @@
+/*
+* Alexandre Bitelo
+* Primeiro comentário
+*/ 
+
+console.log ("Olá Mundo!");
+//prompt ("Digite seu nome: ");
+
+//Variaveis
+//Const é uma variável que não é possivel mudar seu valor
+/*
+const variavelA = 100;
+const variavelB = variavelA + 50;
+console.log (variavelB);
+
+let numero = 50;
+console.log (numero);
+numero = 80;
+console.log (numero);
+
+const nome = "Alexandre";
+const idade = 18;
+
+\n para pular linha
+
+console.log ("Olá!", "Meu nome é", nome, "e eu tenho", idade, "anos.");
+
+*/
+
+
+//Primeiro Exercício
+const primeiroNome = "Alexandre";
+const sobreNome = "Bitelo";
+const idade = 18;
+let estudante = "Eu sou um estudante";
+
+console.log ("Olá, me chamo", primeiroNome, sobreNome,"tenho", idade, "anos e", estudante);
